ea32c9dd9c35b821e345bd87055200b43a8ee42c
Yuhang <abeyuhang@gmail.com>
2016-06-30 17:58:43 +0800

    update

--- index.js
+++ index.js
@@ -1,103 +1,39 @@
 'use strict';
 
 const crypto = require('crypto');
-const validators = require('./validators.js');
-/**
- * Author: petar bojinov - @pbojinov
- * Github: https://github.com/pbojinov/request-ip
- * Get client IP address
- *
- * Modified by Yuhang Ge<abeyuhang@gmail.com>
- *
- */
-function getClientIp(req) {
+const validators = require('./src/validators');
+const getClientIp = require('./src/getip');
+const converters = {
+  'string'(val) {
+    return val.toString();
+  },
+  'number'(val) {
+    return Number(val);
+  },
+  'boolean'(val) {
+    return !!val && val !== 'false'
+  }
+};
 
-  let headers = req.headers;
-  let ipAddress;
 
-  // x-client-ip
-  if (ipAddress = headers['x-client-ip']) {
-    return ipAddress;
-  } else if (ipAddress = headers['x-forwarded-for']) {
-    // x-forwarded-for header is more common
-    // it may return multiple IP addresses in the format:
-    // "client IP, proxy 1 IP, proxy 2 IP"
-    // we pick the first one
-    return ipAddress.split(',')[0];
-  }
-
-  // x-real-ip
-  // (default nginx proxy/fcgi)
-  else if (ipAddress = headers['x-real-ip']) {
-    // alternative to x-forwarded-for
-    // used by some proxies
-    return ipAddress;
-  }
-
-  // x-cluster-client-ip
-  // (Rackspace LB and Riverbed's Stingray)
-  // http://www.rackspace.com/knowledge_center/article/controlling-access-to-linux-cloud-sites-based-on-the-client-ip-address
-  // https://splash.riverbed.com/docs/DOC-1926
-  else if (ipAddress = headers['x-cluster-client-ip']) {
-    return ipAddress;
-  }
-
-  // x-forwarded
-  else if (ipAddress = headers['x-forwarded']) {
-    return ipAddress;
-  }
-
-  // forwarded-for
-  else if (ipAddress = headers['forwarded-for']) {
-    return ipAddress;
-  }
-
-  // forwarded
-  else if (ipAddress = headers['forwarded']) {
-    return ipAddress;
-  }
-
-  // remote address checks
-  else if (req.connection && req.connection.remoteAddress) {
-    return req.connection.remoteAddress;
-  }
-  else if (req.socket && req.socket.remoteAddress) {
-    return req.socket.remoteAddress;
-  }
-  else if (req.connection && req.connection.socket && req.connection.socket.remoteAddress) {
-    return req.connection.socket.remoteAddress;
-  }
-  else if (req.info && req.info.remoteAddress) {
-    return req.info.remoteAddress;
-  }
-  // return unknown if we cannot find an address
-  else {
-    return 'unknown';
-  }
+function getRandomBytes(size) {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(24, (err, buf) => {
+      if (!err) {
+        resolve(buf);
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
 
-
 function genSessionKey() {
-  return new Promise((resolve, reject) => {
-    let tries = 0;
-    function tryGen() {
-      crypto.randomBytes(24, (err, buf) => {
-        if (!err) {
-          resolve(buf.toString('base64')
+  return getRandomBytes(24).then(buf => {
+    return buf.toString('base64')
             .replace(/\+/g, '-')
             .replace(/\//g, '_')
-            .replace(/=/g, ''));
-        } else {
-          if (tries < 3) {
-            tries++;
-            tryGen()
-          } else {
-            reject(err);
-          }
-        }
-      });
-    }
-    tryGen();
+            .replace(/=/g, '');
   });
 }
 
@@ -110,6 +46,7 @@ function merge(src, dst) {
 
 module.exports = {
   validators,
+  converters,
   isGenerateFunction(obj) {
     return typeof obj === 'function' && obj.constructor.name === 'GeneratorFunction';
   },
@@ -133,5 +70,6 @@ module.exports = {
   },
   merge,
   getClientIp,
-  genSessionKey
+  genSessionKey,
+  getRandomBytes
 };
