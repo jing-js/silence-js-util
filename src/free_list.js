5ca6089ff3aedc7a64b49802f23d78ba9c2c311f
Yuhang <abeyuhang@gmail.com>
2016-08-31 20:02:57 +0800

    update

--- src/free_list.js
+++ src/free_list.js
@@ -2,7 +2,7 @@
 const BUF_SIZE = 1000;
 
 class FreeList {
-  constructor(constructor, max = 10) {
+  constructor(constructor, max = 50) {
     this.constructor = constructor;
     this.max = max * BUF_SIZE;
     this.len = BUF_SIZE;
@@ -13,12 +13,10 @@ class FreeList {
     for(; i < this.len; i++) { // 这里使用的是线性遍历的方法, 使用时间换空间效率
       let obj = this.list[i];
       if (!obj) {
-        // console.log(`new ${this.constructor.name}`);
         obj = new this.constructor(...args);
         this.list[i] = obj;
         return obj;
       } else if (!obj.$$freeListIsUsed) {
-        // console.log(`resuse ${this.constructor.name}`);
         obj.$$freeListInit(...args);
         return obj;
       }
@@ -31,17 +29,13 @@ class FreeList {
       this.list[i] = obj;
       return obj;
     }
-    // 如果 this.max (默认为 10000) 个空间都被用完了, 直接返回新实例。
-    // 同时发起请求用户达到 10000 我擦! 同时在线用户岂不是上千万。。。
+    // 如果 this.max (默认为 50 * 1000) 个空间都被用完了, 直接返回新实例。
+    // 同时发起请求用户达到 50000 我擦!
+    console.log('MAX_FREE_LIST');
     return new this.constructor(...args);
   }
   free(obj) {
     obj.$$freeListFree();
-    if (this.list.length < this.max) {
-      this.list.push(obj);
-      return true;
-    }
-    return false;
   }
 }
 
